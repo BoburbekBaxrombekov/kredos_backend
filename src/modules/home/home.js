a9fcc8fece5dace87e6b10753a98828c2c1108c5
Boburbek <boburbaxrombekov@gmail.com>
2022-02-13 10:29:53 +0500

    commit

--- src/modules/home/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    GET: async(req, res) => {
-        try {
-            res.redirect('http://localhost:3000/')
-        } catch(err) {
-            console.log(err.message)
-        }
-    }
-}
