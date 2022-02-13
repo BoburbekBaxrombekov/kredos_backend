41ddcc03c194315e545c7a6921b5e7207179c1ed
BoburbekBaxrombekov <boburbaxrombekov@gmail.com>
2022-02-13 11:10:47 +0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = process.env.port || 4000
+const PORT = process.env.PORT || 4000
 const router = require('./src/modules/router')
 const cors = require('cors')
 
